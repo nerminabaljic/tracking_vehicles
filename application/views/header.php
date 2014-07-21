5fd9786de445f51bb0203c0dd692b63ecc7a502d
KristinaCarapina <kristinacarapina@hotmail.com>
2014-07-21 12:13:10 +0200

    kristina

---
+++ application/views/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> GPS pracenje vozila </title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/css/header_main.css">
+    <meta charset= "utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<div id = "header_container">
+    <header>
+        <figure>
+            <img src="<?php echo base_url();?>/images/heading.png" alt="GPS">
+        </figure>
+    </header>
+</div>
+
+
