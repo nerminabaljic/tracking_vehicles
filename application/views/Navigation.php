7d62b868d1c20029f9a0578f20af673e59f4cca8
Ruza <rushka.mo@gmail.com>
2014-07-23 00:58:16 +0200

    Ruža

--- application/views/Navigation.php
+++ application/views/Navigation.php
@@ -10,19 +10,19 @@
 </head>
 
 <body>
-	
-	<div class = "container">
 
-		<div class ="col-md-2">
-		<ul class= "nav nav-pills nav-stacked">
-				<li class="active"><a href = "<?php echo site_url('main/logirajSe');?>"">Invite employee</a></li>
-                <li><a href="<?php echo site_url('main/korisnici');?>" ">Employee</a></li>
-				<li><a href = "#">Create vehicle</a></li>
-				<li><a href = "<?php echo site_url('main/vehicles');?>">Vehicle</a></li>
-                <li><a href = '<?php echo base_url()."main/logout" ?>'>Logout</a></li>
-		</ul>
-	</div>
+<div class = "container">
 
-<script src="<?php echo base_url();?>/bootstrap3.2/js/bootstrap.min.js"></script>
+    <div class ="col-md-2">
+        <ul class= "nav nav-pills nav-stacked">
+            <li class="active"><a href = "<?php echo site_url('main/logirajSe');?>"">Invite employee</a></li>
+            <li><a href="<?php echo site_url('main/korisnici');?>" ">Employee</a></li>
+            <li><a href = "#">Create vehicle</a></li>
+            <li><a href = "<?php echo site_url('main/vehicles');?>">Vehicle</a></li>
+            <li><a href = '<?php echo base_url()."main/logout" ?>'>Logout</a></li>
+        </ul>
+    </div>
+
+    <script src="<?php echo base_url();?>/bootstrap3.2/js/bootstrap.min.js"></script>
 </body>
 </html>
