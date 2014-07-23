16c3b0231e2582d890e75db4abdc3307afedb2fd
Ruza <rushka.mo@gmail.com>
2014-07-23 09:58:34 +0200

    Ruža

--- js/form_forgot_password.js
+++ js/form_forgot_password.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
-     $("#reset_password").click(function(){    
+    $("#reset_password").click(function(){
         var email = $("#email").val();
-		var data = "email="+email;
+        var data = { email : email };
 
         console.log("email = " + email + ", data = " + data);
 
         $.ajax({
-        method: "POST",
-        //url: "login.php",
-        data: data,
-        success: function(e){
-		
-       //ERROR WILL BE SHOWN IN THE "#error" ELEMENT
-       if(e){
-         $("#error").html(e);
-       } 
-	   else {
-         //GOES THE "CHANGE PASSWORD" FORM UPON LOGGING IN
-         window.location = "url: "login.php"";
-       }  
-       }
-    });	
-});
+            method: "POST",
+            url: "forgot.php",
+            data: data,
+            success: function(response){
+
+                //ERROR WILL BE SHOWN IN THE "#error" ELEMENT
+                if( parseInt(response) == 0){
+                    $("#error").html( "Email adress does not exist" );
+                }
+                else if( parseInt(response) == 1 ){
+                    //GOES THE "CHANGE PASSWORD" FORM UPON LOGGING IN
+                    window.location = "profile.php";
+                }
+            }
+        });
+    });
 });
