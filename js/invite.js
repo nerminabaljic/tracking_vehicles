704c98ec835ac394d79894ea018c91588a49f0ab
KristinaCarapina <kristinacarapina@hotmail.com>
2014-07-23 08:59:52 +0200

    kristina

---
+++ js/invite.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('#add').on('click',function(){
+        var st = '';
+        $('#myForm input[type=text]').each(function(){
+            st = st+ '<td>'+$(this).val()+'</td>';
+            $(this).val('');
+        });
+        st += '<td id="test1" onclick="testFunction(this)">x</td>';
+        $('#tusers').append('<tr>'+st+'</tr>');
+        $('#submit').removeClass('submit');
+    });
+});
+
+function testFunction(td) {
+    td.parentNode.parentNode.removeChild(td.parentNode);
+    $('#submit').addClass('#submit');
+
+}
